99966627e4b1c5d858f32d9977892072168770c5
anton <funaska@dell-e6230-debian.home>
2018-05-16 13:14:43 +0300

    first

---
+++ one.py
@@ -0,0 +1,10 @@
+
+age = int(input('Введите возраст:'))
+if age < 4:
+	print('Иди в детский сад!')
+elif age < 18:
+	print('Иди в школу!')
+elif age < 22:
+	print('Иди в ВУЗ!')
+else:
+	print('Иди работать!')
